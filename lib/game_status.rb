fea4f1430bc34c2df3afa65579449a2b3f6faf9b
Halie Hall <halierae17@outlook.com>
2021-03-29 18:38:39 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -38,5 +38,9 @@ end
 def draw?(board)
   if !won?(board) && full?(board)
     return true
+    elsif !won?(board) && !full(board)
+    return false
+  else won?(board)
+    return false 
   end
 end
